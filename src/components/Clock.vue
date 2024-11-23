e782fb7a76ed75f8e89c916bae4bbc9fd8e7d1a8
bmanth60 <5578150+bmanth60@users.noreply.github.com>
2024-11-22 17:38:57 -0800

    Remove unused code

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,28 +1,3 @@
-export default {
-data() {
-return {
-interval: null,
-time: null
-}
-},
-beforeDestroy() {
-// prevent memory leak
-clearInterval(this.interval)
-},
-created() {
-// update the time every second
-this.interval = setInterval(() => {
-// Concise way to format time according to system locale.
-// In my case this returns "3:48:00 am"
-this.time = Intl.DateTimeFormat(navigator.language, {
-hour: 'numeric',
-minute: 'numeric',
-second: 'numeric'
-}).format()
-}, 1000)
-}
-}
-
 <script lang="ts" setup>
 import { onMounted, onUnmounted } from 'vue'
 import { ref } from 'vue';
